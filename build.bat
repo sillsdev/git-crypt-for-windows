8f655a83a4d45a4d04b95757441b9ac002dc4237
Jim Gr <jimgrady.jg@gmail.com>
2020-12-23 11:44:55 -0500

    Update build script
    Update build script to:
     1. run in the current directory so that Inno Setup knows where the 
    files are located; and
     2. add build of the installer using Inno Setup.

--- build.bat
+++ build.bat
@@ -1,2 +1,12 @@
-copy /Y msys2-build.sh c:\dev\msys64\home\%USERNAME%\msys2-build.sh
-c:\dev\msys64\usr\bin\env MSYSTEM=MINGW64 c:\dev\msys64\usr\bin\bash -l -c "cd /home/%USERNAME%; chmod +x msys2-build.sh ; ./msys2-build.sh"
+REM Set HOME to the current directory so that it is the current directory for the
+REM msys2 shell 
+@echo off
+set HOME=.
+IF NOT EXIST Installer mkdir Installer
+c:\dev\msys64\usr\bin\env MSYSTEM=MINGW64 c:\dev\msys64\usr\bin\bash -l -c "chmod +x msys2-build.sh ; ./msys2-build.sh"
+
+REM Create the installer package
+setlocal
+set Path=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
+
+iscc.exe .\git-crypt-setup.iss
